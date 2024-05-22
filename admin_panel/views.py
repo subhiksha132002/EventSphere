d80f2e15f7c6ef0c05a40803c8d2010a961bcb19
Subhiksha B <subhiksha132002@gmail.com>
2024-05-22 19:15:01 +0530

    NoneType error for User resolved

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -14,7 +14,7 @@ from django.views.generic import View
 from .decorators import admin_required
 
 
-@admin_required
+
 # Views for Admin Dashboard
 def dashboard_view(request):
     # Retrieve data for the dashboard
@@ -35,13 +35,12 @@ def dashboard_view(request):
     }
     return render(request, 'index.html',context)
 
-@admin_required
 # Function to retrieve event page
 def events_view(request):
     all_events = Event.objects.all()
     return render(request, 'events.html', {'active_page': 'events', 'all_events': all_events})
 
-@admin_required
+
 def create_event(request):
     event_categories = EventCategory.objects.all()  # Retrieve all event categories
     attendees = CustomUser.objects.all()  # Retrieve all attendees
@@ -69,7 +68,7 @@ def create_event(request):
         'active_page': 'events'
     })
 
-@admin_required
+
 def edit_event_view(request, event_id):
     # Retrieve existing event
     event = get_object_or_404(Event, id=event_id)
@@ -88,19 +87,19 @@ def edit_event_view(request, event_id):
     context = {'form': form, 'event': event, 'active_page': 'events'}
     return render(request, 'edit_event.html', context)
 
-@admin_required
+
 def delete_event(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     event.delete()
     return redirect('admin_panel:events')  # Redirect to the events page after deletion
 
-@admin_required
+
 # Function to retrieve event category page
 def event_categories_view(request):
     all_categories = EventCategory.objects.all()
     return render(request, 'event_categories.html', {'active_page': 'event_categories', 'all_categories': all_categories})
 
-@admin_required
+
 def create_category(request):
     if request.method == 'POST':
         form = EventCategoryForm(request.POST)
@@ -112,7 +111,7 @@ def create_category(request):
     active_page = 'event_categories'
     return render(request, 'create_category.html', {'form': form, 'active_page': active_page})
 
-@admin_required
+
 def edit_category(request, category_id):
     category = get_object_or_404(EventCategory, id=category_id)
     if request.method == 'POST':
@@ -125,14 +124,14 @@ def edit_category(request, category_id):
     active_page = 'event_categories'
     return render(request, 'edit_category.html', {'form': form, 'category': category, 'active_page': active_page})
 
-@admin_required   
+  
 def delete_category(request, category_id):
     category = get_object_or_404(EventCategory, id=category_id)
     category.delete()
     # Redirect to a success page or another appropriate URL
     return redirect('admin_panel:event_categories')
 
-@admin_required
+
 # Function to retrieve user page
 def users_view(request):
     # Retrieve all CustomUser objects along with related data
@@ -144,14 +143,15 @@ def users_view(request):
     }
     
     return render(request, 'attendees.html', context)
-@admin_required
+
+
 def create_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             if user.username:  # Ensure username is provided and not empty
-                events_attending = form.cleaned_data.get('events_attending')
+                events_attending = form.cleaned_data.get('events_attending', [])
                 user.save()
                 user.events_attending.set(events_attending)
                 success_message = 'User created successfully.'
@@ -174,7 +174,7 @@ def create_user(request):
     }
     return render(request, 'create_attendee.html', context)
 
-@admin_required
+
 def edit_user(request, user_id):
     user = get_object_or_404(CustomUser, id=user_id)
 
@@ -182,7 +182,7 @@ def edit_user(request, user_id):
         form = EditUserForm(request.POST, instance=user)
         if form.is_valid():
             user = form.save(commit=False)
-            events_attending = form.cleaned_data.get('events_attending')
+            events_attending = form.cleaned_data.get('events_attending', [])
             user.save()
             user.events_attending.set(events_attending)
             success_message = 'User updated successfully.'
@@ -198,13 +198,12 @@ def edit_user(request, user_id):
     }
     return render(request, 'edit_user.html', context)
 
-@admin_required
 def delete_user(request, user_id):
     user = get_object_or_404(User, id=user_id)
     user.delete()
     return redirect('admin_panel:users')
 
-@admin_required
+
 def event_organizers_view(request):
     active_page = None
     organizers_with_events = {}  # Initialize an empty dictionary to store organizers and their events
@@ -224,7 +223,7 @@ from django.http import JsonResponse
 
 User = get_user_model()
 
-@admin_required
+
 def create_organizer(request):
     if request.method == 'POST':
         form = EventOrganizerForm(request.POST)
@@ -252,7 +251,7 @@ def create_organizer(request):
         active_page = 'event_organizers'
         return render(request, 'create_organizer.html', {'form': form, 'active_page': active_page})
 
-@admin_required           
+          
 def edit_organizer(request, organizer_id):
     organizer = get_object_or_404(EventOrganizer, id=organizer_id)
 
@@ -274,7 +273,7 @@ def edit_organizer(request, organizer_id):
     }
     return render(request, 'edit_organizer.html', context)
 
-@admin_required
+
 def delete_organizer(request, organizer_id):
     organizer = get_object_or_404(EventOrganizer, id=organizer_id)
     organizer.delete()
